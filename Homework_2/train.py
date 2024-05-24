89b9107b0efc5d8b3223c6dfbfd2c70484328bac
Megan Guidry <meganguidry22@gmail.com>
2024-05-24 17:19:18 +0100

    did some homework

--- Homework_2/train.py
+++ Homework_2/train.py
@@ -1,9 +1,11 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
 
 
 def load_pickle(filename: str):
@@ -22,6 +24,10 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
+# added mlflow
+with mlflow.start_run():
+    mlflow.RandomForestRegressor.autolog()
+    
     rf = RandomForestRegressor(max_depth=10, random_state=0)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_val)
