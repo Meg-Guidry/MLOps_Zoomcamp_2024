22d946c5993b11c9b2617c5160eca0ca3636eb7f
Megan Guidry <meganguidry22@gmail.com>
2024-05-27 08:59:08 -0600

    homework progress

--- Homework_2/train.py
+++ Homework_2/train.py
@@ -6,7 +6,8 @@ import mlflow
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("Homework_2_experiment")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -15,7 +16,7 @@ def load_pickle(filename: str):
 
 @click.command()
 @click.option(
-    "--data_path",
+    "--data_path", 
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
@@ -24,15 +25,15 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-# added mlflow
-with mlflow.start_run():
-    mlflow.RandomForestRegressor.autolog()
+    # added mlflow
+    with mlflow.start_run():
+        mlflow.sklearn.autolog()
+        
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
     
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
